c2d0a957c7a535fd5fa2ebe3de1ed9a0c1145f1f
Tobest82 <emmanueltc899@gmailcom>
2026-03-13 16:13:10 +0100

    Fix(types): sitemap: guard lastmod/priority checks and cast map param to any

--- src/app/sitemap.xml.tsx
+++ src/app/sitemap.xml.tsx
@@ -51,10 +51,10 @@ export async function GET() {
     ...sellers,
   ];
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${all.map((r) => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${all.map((r: any) => {
     const url = `${baseUrl}/${r.loc}`.replace(/([^:])\/\//g, "$1/");
-    const lastmod = r.lastmod ? `<lastmod>${r.lastmod}</lastmod>` : "";
-    const priority = r.priority ? `<priority>${r.priority}</priority>` : "";
+    const lastmod = (r && r.lastmod) ? `<lastmod>${r.lastmod}</lastmod>` : "";
+    const priority = (r && r.priority) ? `<priority>${r.priority}</priority>` : "";
     return `<url><loc>${url}</loc>${lastmod}${priority}</url>`;
   }).join("\n")}\n</urlset>`;
 
